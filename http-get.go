f4046e1b5d07ea638092441433f90820d80b721a
yashaskv9480 <yashu9480@gmail.com>
2025-12-05 01:09:06 +0530

    Parse json

--- http-get.go
+++ http-get.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 )
 
+//https://mholt.github.io/json-to-go/
+type CatResponse struct { //For json parse
+	Fact   string `json:"fact"`
+	Length int    `json:"length"`
+}
+
 func Perform_http_call() {
 	// URL := os.Getenv("URL")
-	URL := "https://dummyjson.com/products"
+	URL := "https://catfact.ninja/fact"
 
 	// url, err := url.ParseRequestURI(URL)
 
@@ -19,8 +27,31 @@ func Perform_http_call() {
 		os.Exit(1)
 	}
 
-	response, _ := http.Get(URL)             //stream response
-	ioOutput, _ := io.ReadAll(response.Body) // Real all to buffer
+	response, err := http.Get(URL) //stream response
+
+	if err != nil || response.StatusCode != 200 {
+		var logInfo any
+
+		if err != nil {
+			logInfo = err
+		} else {
+			logInfo = response.Body
+		}
+		log.Fatal(logInfo) //exits
+	}
+	fmt.Print("hii")
+	ioOutput, _ := io.ReadAll(response.Body) // Read all to buffer
 	StringOutput := string(ioOutput)         // Convert to String
-	fmt.Print(StringOutput)
+
+	log.Println("String is ", StringOutput)
+
+	var jsonOutput CatResponse
+	err = json.Unmarshal(ioOutput, &jsonOutput) //Convert to Json Object
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Json Result is ", jsonOutput)
+	defer response.Body.Close()
 }
